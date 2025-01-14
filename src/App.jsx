55990553cb4f1f6ed0ebc3dda3f62b850cbc4e52
rinaldial11 <rinaldilengkana44@gmail.com>
2025-01-14 18:18:11 +0000

    fixing routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
-import { useState } from 'react'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import './App.css'
-import Home from './page/Home.jsx';
-import Topup from './page/Top-up.jsx';
-import Register from './pages/Register.jsx';
+import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home.jsx";
+import Topup from "./pages/Top-up.jsx";
+import Register from "./pages/Register.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Home />,
   },
   {
-    path: '/top-up',
+    path: "/top-up",
     element: <Topup />,
   },
   {
-    path: '/register',
+    path: "/register",
     element: <Register />,
   },
 ]);
 function App() {
-  useState(0)
-  return (
-    <RouterProvider router={router} />
-  )
+  useState(0);
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
