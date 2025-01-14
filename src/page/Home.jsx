0b371ebb5a693f9ed35352ff46948a1a9fcf6492
rinaldial11 <rinaldilengkana44@gmail.com>
2025-01-14 16:27:55 +0000

    fix imported navbar

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 
 function Home() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+      <div>
+        <Navbar />
+      </div>
+    </>
   );
 }
 
