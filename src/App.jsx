d7e37e2cf72497913d7037050a9c0846b5997efc
Endra Dwi <endradwijamludin@gmail.com>
2025-01-14 21:32:56 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import './App.css'
+import Home from './page/Home.jsx';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+]);
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <button className='btn btn-primary text-info'> Click me</button>
-    <button className='btn btn-secondary'> Click me</button>
-    <button className='btn bg-abuMuda'> Click me</button>
-    <button className='btn btn-neutral'> Click me</button>
-    <button className='btn btn-info'> Click me</button>
-    <button className='btn btn-success'> Click me</button>
-    <button className='btn btn-warning'> Click me</button>
-    </>
+        <RouterProvider router={router} />
   )
 }
 
 export default App
-
