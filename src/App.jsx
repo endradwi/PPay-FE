e5c76ab274822e410283dc8236f781e2cf9843f1
NandaHasnan <nandahasnan6@gmail.com>
2025-01-15 00:55:31 +0700

    topup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import { useState } from 'react'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import './App.css'
 import Home from './page/Home.jsx';
+import Topup from './page/Top-up.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
   },
+  {
+    path: '/top-up',
+    element: <Topup />,
+  },
 ]);
 function App() {
-
+  useState(0)
   return (
         <RouterProvider router={router} />
   )
