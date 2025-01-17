a29ccce14ac6ec15232fe5355ab97adeffec0cf0
rinaldial11 <rinaldilengkana44@gmail.com>
2025-01-17 02:10:05 +0000

    feat: integration register page with backend api

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,30 @@
 import React from "react";
+
 import ppay from "../assets/icons/ppay.svg";
+
 import { RxHamburgerMenu } from "react-icons/rx";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <>
-      <nav className="flex drawer drawer-end justify-between navbar bg-primary px-5 md:px-12 lg:px-32 py-4 shadow-md">
+      <nav className="flex navbar justify-between bg-primary px-5 md:px-12 lg:px-32 py-4 shadow-md">
         <img className="h-8" src={ppay} alt="ppay" />
         <div className="md:flex gap-6 hidden">
-          <button className="btn text-neutral bg-primary border border-neutral text-sm font-normal">
+          <Link
+            to={"/login"}
+            className="btn text-neutral bg-primary border border-neutral text-sm font-normal"
+          >
             Sign In
-          </button>
-          <button className="btn bg-neutral border-none text-primary text-sm font-normal">
+          </Link>
+          <Link
+            to={"/register"}
+            className="btn bg-neutral border-none text-primary text-sm font-normal"
+          >
             Sign Up
-          </button>
+          </Link>
         </div>
-        <div className="block text-neutral text-2xl md:hidden">
+        <div className="text-neutral md:hidden block text-2xl">
           <RxHamburgerMenu />
         </div>
       </nav>
