99784a6e54ad8c5216dcb0a750989254454a1201
Endra Dwi <endradwijamludin@gmail.com>
2025-01-21 14:34:53 +0700

    update delete profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -68,6 +68,27 @@ function Profile() {
       });
   }
 
+  function deletedProfile(params) {
+    fetch(`${API_URL}/users/:id`,{
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+    })
+    .then((res)=>res.json())
+    .then(()=>{
+      setnotDeleted(!notdeleted)
+    })
+  }
+
+  function logout() {
+    setTimeout(() => {
+      setProfile({});
+      setToken("");
+      
+    }, 2000);
+  }
+
   function setImage(e) {
     const selected = e.target.files[0];
     const formData = new FormData();
@@ -170,38 +191,6 @@ function Profile() {
                 </div>
                 <div className="font-semibold text-base">Profile</div>
               </div>
-                {isOpen && (
-                  <div role="alert" className="alert alert-success">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="text-white h-6 w-6 shrink-0 stroke-current"
-                    fill="none"
-                    viewBox="0 0 24 24">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                  </svg>
-                  <span className="text-white">Profile updated successfully</span>
-                </div>
-                )}
-                {isOpen2 && (
-                  <div role="alert" className="alert alert-error">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="text-white h-6 w-6 shrink-0 stroke-current"
-                    fill="none"
-                    viewBox="0 0 24 24">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                  </svg>
-                  <span className="text-white">Error! Task failed successfully.</span>
-                </div>
-                )}
             </div>
             <div className="flex gap-8">
               <form
