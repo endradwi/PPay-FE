72714c5531c2eefc323e3be945d479648734de5c
Endra Dwi <endradwijamludin@gmail.com>
2025-01-21 14:00:52 +0700

    Update notif profile & delete profile

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -47,8 +47,8 @@ function Register() {
           setMessage(v.message);
         }
         if (v.status === 201) {
-          setValid(!valid)
-          setMessage(v.message)
+          setValid(!valid);
+          setMessage(v.message);
         }
       });
   }
@@ -58,12 +58,11 @@ function Register() {
     handleSubmit,
     formState: { errors },
   } = useForm({ resolver: yupResolver(regisValidationSchema) });
-  
 
   React.useEffect(() => {
     if (status === 201) {
       setTimeout(() => {
-        setValid(!valid)
+        setValid(!valid);
         navigate("/login");
       }, 1000);
       return;
@@ -107,24 +106,24 @@ function Register() {
               </div>
             </div>
             {valid && (
-                <>
+              <>
                 <div role="alert" className="alert alert-success text-neutral">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 shrink-0 stroke-current"
+                    class="h-6 w-6 shrink-0 stroke-current"
                     fill="none"
                     viewBox="0 0 24 24"
                   >
                     <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                     />
                   </svg>
                   <span className="text-neutral">Register Succes</span>
                 </div>
-              </>              
+              </>
             )}
             {status === 400 && (
               <>
